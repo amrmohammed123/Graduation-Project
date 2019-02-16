491e6030979cd536e6088e51420d417c844ae81e
amrmohammed123 <amrmohammed362@gmail.com>
2019-02-16 13:10:51 +0200

    Add files via upload

---
+++ Decompile .py
@@ -0,0 +1,7 @@
+import timeit
+import os
+
+#put the folders coming from zip in form drebin-0 , drebin-1, drebin-2 , drebin-3, drebin-4, drebin-5
+for i in range(0,6):
+    for filename in os.listdir("drebin-"+str(i)):
+        os.system('apktool d ' + os.path.join("drebin-"+str(i),filename) +' -o '+ os.path.join('output',"drebin-"+str(i),filename))
